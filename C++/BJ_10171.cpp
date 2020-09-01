1d27900867743c938c73e974365d09d2f57d664a
Choi Jimin <ewqazxc319@gmail.com>
2020-09-01 22:58:15 +0900

    BJ_10171

---
+++ C++/BJ_10171.cpp
@@ -0,0 +1,10 @@
+//BJ_10171
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|";
+}
