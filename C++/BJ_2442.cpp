398e6b3999d5b0f4048da1d9f839e216d9ed5eda
Choi Jimin <ewqazxc319@gmail.com>
2020-09-07 23:33:08 +0900

    BJ_2442

--- C++/BJ_2442.cpp
+++ C++/BJ_2442.cpp
@@ -7,12 +7,8 @@ int main() {
 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 5-i; j++) {
-			cout << " ";
-		}
-		for (int j = 0; j < 2 * i + 1; j++) {
-			cout << "*";
-		}
+		for (int j = 0; j < n - i - 1; j++) { cout << " "; }
+		for (int j = 0; j < 2 * i + 1; j++) { cout << "*"; }
 		cout << "\n";
 	}
 }
