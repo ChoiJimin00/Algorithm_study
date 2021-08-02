4fea3c8cfbb17ce9dbbfce98add6fed77674ad6f
Zimin <ewqazxc319@gmail.com>
2021-08-03 00:22:32 +0900

    GCD&LCM

--- Python/GCD.py
+++ Python/GCD.py
@@ -1,8 +1,21 @@
-# greatest common divisor
-a,b = map(int, input().split())
+def GCD():
+    a,b = map(int, input().split())
 
-# 나머지가 0일때 까지
-while b != 0:
-    a,b = b, a % b
+    # 나머지가 0일때 까지
+    while b != 0:
+        a,b = b, a % b
 
-print(a)
+    print(a)
+
+def LCM():
+    a, b = map(int, input().split())
+    A, B = a, b
+
+    while b != 0:  # 유클리드 호제법
+        a, b = b, a % b
+
+    print(A*B//a)
+
+
+GCD()
+LCM()
