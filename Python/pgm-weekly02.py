1ad00dab475f13e2b0dfcfdf718b024e42fb9556
Zimin <ewqazxc319@gmail.com>
2021-08-12 22:54:55 +0900

    pgm-weekly02

--- Python/pgm-weekly02.py
+++ Python/pgm-weekly02.py
@@ -1,8 +1,47 @@
-def solution(price, money, count):
-    mon = [price * (cnt + 1) for cnt in range(count)]
+import numpy as np
 
-    cost = sum(mon) - money
-    if cost > 0:
-        return cost
+scores = [[50,90],[50,87]]
+
+def get_grade(grade):
+    if grade >= 90:
+        return 'A'
+    if grade >= 80:
+        return 'B'
+    if grade >= 70:
+        return 'C'
+    if grade >= 50:
+        return 'D'
     else:
-        return 0
+        return 'F'
+
+
+def solution(scores):
+    le = len(scores)
+
+    lank = []
+    final = []
+
+    for i in range(le):
+        for j in range(le):
+            lank.append(scores[j][i])
+
+        large = max(lank)
+        small = min(lank)
+
+        if large == scores[i][i]:  # 최대
+            if lank.count(large) > 1:  # 유일 X
+                final.append(get_grade(np.mean(lank)))
+            else:
+                lank.remove(large)
+                final.append(get_grade(np.mean(lank)))
+
+        else:
+            if lank.count(small) > 1:  # 유일 X
+                final.append(get_grade(np.mean(lank)))
+            else:
+                lank.remove(small)
+                final.append(get_grade(np.mean(lank)))
+
+        lank = []
+
+    return (''.join(final))
