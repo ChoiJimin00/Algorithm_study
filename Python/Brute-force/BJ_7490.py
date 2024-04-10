65784a50f6f3d9025264fec5d59068b79c0a927c
jmchoi <ewqazxc319@gmail.com>
2024-04-10 15:37:10 +0900

    Solve BOJ 7490(Brute Force problem)

---
+++ Python/Brute-force/BJ_7490.py
@@ -0,0 +1,39 @@
+import sys
+import copy
+input = sys.stdin.readline
+
+def recur(arr, m):
+    global formular
+    if len(arr) == m:
+        formular.append(copy.deepcopy(arr))
+        return
+
+    arr.append(' ')
+    recur(arr,m)
+    arr.pop()
+    
+    arr.append('+')
+    recur(arr,m)
+    arr.pop()
+    
+    arr.append('-')
+    recur(arr,m)
+    arr.pop()
+
+
+if __name__ == '__main__':
+    N = int(input())
+    
+    for _ in range(N):
+        formular = []
+        m = int(input())
+        recur([],m-1)
+        
+        for form in formular:
+            statement = ''
+            for i in range(m-1):
+                statement += str(i+1) + form[i]
+            statement += str(m)
+            if (eval(statement.replace(' ',''))) == 0:
+                print(statement) 
+        print()       
