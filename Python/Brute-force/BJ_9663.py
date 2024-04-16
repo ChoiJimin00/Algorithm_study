3165ee05f0adfb505609ccdbdc7e19712561bc12
jmchoi <ewqazxc319@gmail.com>
2024-04-16 21:07:09 +0900

    Solve BOJ 9663 (brute-force problem)

---
+++ Python/Brute-force/BJ_9663.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cnt = 0
+row = [0]*N
+
+def chk_attack(x):
+    # 상하좌우, 대각선에 퀸이 있는지 체크
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x]-row[i]) == abs(x-i): # 같은 행에 퀸이 있거나, 대각선에 퀸이 있는 경우
+            return True
+    return False
+        
+def dfs(start):
+    global cnt 
+    
+    if start == N :
+        cnt += 1
+    
+    else:
+        for i in range(N):
+            row[start] = i
+            if not chk_attack(start):
+                dfs(start+1)
+
+dfs(0)
+print(cnt)
